0bfd43f7333ad59ad7a1974dc9093a2859b8f86e
Guerra <wesley.moreira@vetta.digital>
2022-01-30 19:03:12 -0300

    Chore: Update config redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
 import "./App.css";
-import { useSelector } from "react-redux";
-import { RootState } from "./redux/store";
+// import { useSelector } from "react-redux";
+// import { RootState } from "./redux/store";
+import { setName, setAge } from "./redux/reducers/useReducer";
+import { useAppSelector } from "./redux/hooks/useAppSelector";
+import { useDispatch } from "react-redux";
 
 function App() {
-  const user = useSelector((state: RootState) => state.user);
-  // dataGeral.user = user;
+  const dispatch = useDispatch();
+
+  const changeName = (newName: string) => {
+    dispatch(setName(newName));
+  };
+
+  const handleNameInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    changeName(e.target.value);
+  };
+
+  const handleAgeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch(setAge(e.target.value)); // dispatch(setAge(e.target.value));
+  };
+  // user is a redux state , pegando a partir do store do redux
+  // const user = useSelector((state: RootState) => state.user);
+  // dataGeral.user.name
+  const user = useAppSelector((state) => state.user); // pegando o user do state do redux
   return (
     <div>
       Meu nome é: {user.name} e tenho {user.age} anos. <br />
       <hr />
-      <input type="text" value={user.name} />
+      <input type="text" value={user.name} onChange={handleNameInput} />
+      <input type="text" value={user.age} onChange={handleAgeInput} />
       <hr />
       <button>Switch Theme</button>
     </div>
